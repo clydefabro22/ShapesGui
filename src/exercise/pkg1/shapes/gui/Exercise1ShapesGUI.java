34a5455c6179ae109b55495da5e4e19d592fd215
clydefabro22 <115932330+clydefabro22@users.noreply.github.com>
2023-11-28 15:29:40 +0800

    Update Exercise1ShapesGUI.java

--- src/exercise/pkg1/shapes/gui/Exercise1ShapesGUI.java
+++ src/exercise/pkg1/shapes/gui/Exercise1ShapesGUI.java
@@ -1,9 +1,10 @@
-//package exercise.pkg1.shapes.gui;
-//
-//public class Exercise1ShapesGUI {
-//
-//    public static void main(String[] args) {
-//        
-//    }
-//    
-//}
+package exercise.pkg1.shapes.gui;
+
+public class Exercise1ShapesGUI {
+
+    public static void main(String[] args) {
+        ShapesGUI f = new ShapesGUI();
+        f.setVisible(true);
+    }
+    
+}
